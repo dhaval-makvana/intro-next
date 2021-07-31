48ebb48a49b07afd6e4560d0b834f01ce3829daa
Dhaval Nykaa <dhavalkumar.makvana@nykaa.com>
2021-07-31 18:15:46 +0530

    feat: Page navigation with Link component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,15 @@
+// lib
 import React from "react";
+import Link from "next/link";
 
-const Page = () => <div>Index Page</div>;
+const Page = () => {
+    return (
+        <div>
+            <h1>Main App Home Page</h1>
+            <Link href="/notes"><a>Notes Page</a></Link>
+            <h2>Docs Page</h2>
+        </div>
+    )
+};
 
 export default Page;
--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -1,5 +1,25 @@
+// lib
 import React from "react";
+import Link from "next/link";
 
-const AllNotes = () => <div>All Notes Page</div>;
+const AllNotes = () => {
+    const notes = new Array(15).fill(1).map((e, i) => ({id: i, title: `Note: ${i}`}))
+
+  return (
+    <div>
+      <h1>Notes</h1>
+
+      {notes.map(note => (
+        <div>
+          <Link key={note.id} href="/notes/[id]" as={`/notes/${note.id}`}>
+            <a>
+              <strong>{note.title}</strong>
+            </a>
+          </Link>
+        </div>
+      ))}
+    </div>
+  )
+};
 
 export default AllNotes;
